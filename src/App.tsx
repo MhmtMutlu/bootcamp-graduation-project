716929c42bce214da756310e9519a82c0ee4fb0f
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-08-29 00:10:07 +0300

    Feat: LoginContextProvider was used in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ComplaintContextProvider from "./context/ComplaintContext";
+import LoginContextProvider from "./context/LoginContext";
 import Routes from "./routes/Routes";
 
 function App() {
   return (
     <ComplaintContextProvider>
-      <Routes />
+      <LoginContextProvider>
+        <Routes />
+      </LoginContextProvider>
     </ComplaintContextProvider>
   );
 }
