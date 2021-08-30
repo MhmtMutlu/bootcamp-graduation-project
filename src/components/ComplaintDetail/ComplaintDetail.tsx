4efc6b44eb9d195cb77f0af687a9d4c711248df4
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-08-30 15:05:17 +0300

    Feat: ComplaintDetail was directed to ComplaintList after admin saved response.

--- src/components/ComplaintDetail/ComplaintDetail.tsx
+++ src/components/ComplaintDetail/ComplaintDetail.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { IProps, IUpdataValue } from "../../types";
 import { ButtonWrapper, ErrorMessage, InputField, SaveButton, Title } from "./styles";
@@ -12,9 +13,11 @@ function ComplaintDetail({ adminResponse, id }:IProps) {
     register,
     formState: { errors },
   } = useForm<IUpdataValue>({ resolver: yupResolver(DetailSchema) });
+  const history = useHistory();
 
   const onSubmit = (data: IUpdataValue) => {
     updateComplaints(data, id);
+    history.push("/admin/complaint-list");
   };
 
   return (
