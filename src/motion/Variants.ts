2c550f99d6b47ac01a4aa2ebb0f04f6cc39e1b5e
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-08-30 18:59:19 +0300

    UI-Feat: Motion folder was created for animations.

---
+++ src/motion/Variants.ts
@@ -0,0 +1,41 @@
+export const card = {
+  hidden: {
+    y: 25,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+  }
+};
+
+export const complaintCards = {
+  hidden: {
+    opacity: 0
+  },
+  show: {
+    opacity: 1,
+    transition: { staggerChildren: 0.05, delayChildren: 0.09 },
+  },
+};
+
+export const homePageItem = {
+  hidden: {
+    x: -20,
+    opacity: 0,
+  },
+  show: {
+    x: 0,
+    opacity: 1,
+  }
+};
+
+export const homePageItems = {
+  hidden: {
+    opacity: 0
+  },
+  show: {
+    opacity: 1,
+    transition: { staggerChildren: 0.05, delayChildren: 0.09 },
+  },
+};
